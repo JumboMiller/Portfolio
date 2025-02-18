d35b34ca1631b6de7a305939f25d78dd8ba0c9fd
Jumbo <damirchot@gmail.com>
2025-02-18 22:16:38 +0100

    new component LoadingMacBokk

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,6 @@ import Experience from "@/features/experience-section/Experience"
 import Hero from "@/features/hero-section/Hero"
 import Projects from "@/features/project-section/Projects"
 import Skills from "@/features/skills-section/Skills"
-import LoadingMacBook from "@/shared/components/LoadingMacBook/LoadingMacBook"
 
 
 const Index = () => {
@@ -10,7 +9,6 @@ const Index = () => {
   return (
     <main className="content">
       <div className="content__section">
-        <LoadingMacBook/>
         <Hero/>
         <Skills/>
         <Experience/>
